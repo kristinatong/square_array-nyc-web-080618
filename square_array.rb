dcd58d570e0d467715497d9952b0777bfbd6e282
Kristina Tong <kristina.tong@gmail.com>
2018-07-31 00:53:39 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,14 +3,5 @@ def square_array(array)
   array.each do |num|
     new_array.push(num*num)
   end
+  new_array
 end
-
-square_array([1,2,3,4])
-
-def square_array_collect(array)
-  array.collect do |num|
-    num*num
-  end
-end
-
-square_array_collect([1,2,3,4])
