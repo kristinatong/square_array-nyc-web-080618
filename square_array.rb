0e10a4417c7f0b7e760d56c20584fa1e8660d2b4
Kristina Tong <kristina.tong@gmail.com>
2018-07-31 00:46:44 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   new_array = []
   array.each do |num|
-    new_array.push(Math.sqrt(num))
+    square_root = Math.sqrt(num)
+    new_array.push(square_root)
   end
   new_array
 end
